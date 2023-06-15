a8083b4c6710e215993b7129ddc25cfb8ce2221b
Maniwell <emailtoamanuel@gmail.com>
2023-06-15 20:33:49 +0000

    Added the contents for the mandatory projects

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* Main - Main entry point
+* The program prints string of characters using the printf function
+*
+* Return success - Always returns 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
