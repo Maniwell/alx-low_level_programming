49ace75f0c8dc674421184bdc72881265699b248
Maniwell <emailtoamanuel@gmail.com>
2023-06-20 17:31:35 +0000

    Added files for projects 5 and 6

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+  * print_sign - diffrentiates if the input integer is
+  * negative, positive or zero
+  * @n: interger to be checked
+  * Return: 0 (zero), 1 (positive), and -1 (negative)
+  **/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+}
+
