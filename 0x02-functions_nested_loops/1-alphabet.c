b01727c35b1c23f351b50629616baedbfc4c454d
Maniwell <emailtoamanuel@gmail.com>
2023-06-20 16:54:12 +0000

    Added files for projects 3 and 4

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-  *main - calls the function print_alphabet to
+  * print_alphabet - calls the function print_alphabet to
   * print lower case letters of the alphabet.
   * print_alphabet - writes the lowercase letter of the
   * alphabet
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+  *_isalpha - checks if the input is in the alphabet
+  * @c: is the character to be checked
+  * Return: if 1 success or if 0 not in the alphabet
+  **/
+
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
