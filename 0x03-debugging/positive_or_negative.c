06bc30fcea4403096527f56a10d2e957bcbcbb4d
Maniwell <emailtoamanuel@gmail.com>
2023-06-21 14:21:47 +0000

    Debugging first project added with ammends

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,29 +1,26 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
   * positive_or_negative - The function assigns the values of positive,
-  * negative and zero for an input of integer variable n.
-  * @n: is an integer input to be checked
+  * negative and zero for an input of integer variable i.
+  * @i: is an integer input to be checked
   * Return: Always 0 (Success)
   **/
 
-void positive_or_negative(int n)
+void positive_or_negative(int i)
 {
-	int n;
 
-	if (n > 0)
+	if (i > 0)
 	{
-		printf("%d is positive\n", n);
+		printf("%d is positive\n", i);
 	}
-	else if (n < 0)
+	else if (i < 0)
 	{
-		printf("%d is negative\n", n);
+		printf("%d is negative\n", i);
 	}
 	else
 	{
-		printf("%d is zero\n", n);
+		printf("%d is zero\n", i);
 	}
 }
