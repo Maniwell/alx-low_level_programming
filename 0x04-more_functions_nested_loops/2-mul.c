d3da5708ea4b9a98912a3d485a4f02711d489469
Maniwell <emailtoamanuel@gmail.com>
2023-06-22 18:22:48 +0000

    added files upto project 6 for more functions project

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+  * mul - gives the multiple of two input integers
+  * @i: first integer input
+  * @j: second integer input
+  * Return: integer of the multiplicattion
+  */
+
+int mul(int i, int j)
+{
+	return (i * j);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directroy contains files related to the projects in 0x04-more functions nested loops, ALX SE.
