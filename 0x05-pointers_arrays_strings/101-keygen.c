3ded133a5d6fcbe5fadcac35910c7caa6c02b383
Maniwell <emailtoamanuel@gmail.com>
2023-06-26 21:39:30 +0000

    advanced task 2 added final

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,34 +3,32 @@
 #include <time.h>
 
 /**
-   * main - is a funtion that generates random password
-    *
-     * Return: Always 0 (Success)
-      */
+  * main - is a funtion that generates random password
+  *
+  * Return: Always 0 (Success)
+  */
 int main(void)
 {
-		int pass[120];
-			int i, sum, n;
+	int pass[120];
+	int i, sum, n;
 
-				sum = 0;
+	sum = 0;
+	srand(time(NULL));
 
-					srand(time(NULL));
-
-						for (i = 0; i < 120; i++)
-								{
-											pass[i] = rand() % 51;
-													sum += (pass[i] + '0');
-															putchar(pass[i] + '0');
-																	if ((2772 - sum) - '0' < 51)
-																				{
-n = 2772 - sum - '0';
+	for (i = 0; i < 120; i++)
+	{
+		pass[i] = rand() % 51;
+		sum += (pass[i] + '0');
+	putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 51)
+		{
+			n = 2772 - sum - '0';
 			sum += n;
-																	putchar(n + '0');
-																break;
-													}
-																		}
+			putchar(n + '0');
+			break;
+		}
+	}
+	putchar('\n');
 
-						    putchar('\n');
-
-						    	return (0);
+	return (0);
 }
