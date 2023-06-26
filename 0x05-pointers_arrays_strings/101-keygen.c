212a721f08ec8f40f082f573edf7facc3de9b795
Maniwell <emailtoamanuel@gmail.com>
2023-06-26 21:22:58 +0000

    advanced task 2 added

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,23 +8,23 @@ int main(void)
 {
 	    char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$^&*()_+}{|:?><|~/.,;'][-=]}";
 	        int len = sizeof(charset) - 1;
-		    char password[PASSWORD_LENGTH + 1];
-		        int sum = 0;
+		    char pass[PASSWORD_LENGTH + 1];
+		        int i, sum = 0;
 
 			    srand(time(NULL));
 
-			        for (int i = 0; i < PASSWORD_LENGTH; i++) {
-					        password[i] = charset[rand() % len];
-						        sum += password[i];
+			        for ( i = 0; i < PASSWORD_LENGTH; i++) {
+					        pass[i] = charset[rand() % len];
+						        sum += pass[i];
 							    }
 
-				    password[PASSWORD_LENGTH] = '\0';
+				    pass[PASSWORD_LENGTH] = '\0';
 
 				        if (sum % 62 != 0) {
-						        password[rand() % PASSWORD_LENGTH] = charset[sum % 62];
+						        pass[rand() % PASSWORD_LENGTH] = charset[sum % 62];
 							    }
 
-					    printf("%s\n", password);
+					    printf("%s\n", pass);
 					        putchar('\n');
 
 						    return 0;
