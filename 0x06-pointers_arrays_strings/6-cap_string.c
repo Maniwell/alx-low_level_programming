a4de8b402213c34746ad6739b287da4f667813b3
Maniwell <emailtoamanuel@gmail.com>
2023-06-29 21:45:02 +0000

    Added mandatory project tasks for 0x06-pointers_arrays_strings ALX SE with ammend

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,30 +2,35 @@
 
 /**
   * cap_string - Capitalizes all words of a string.
-  * @s: The string to modify.
+  * @s: The string to be capitalized.
   *
-  * Return: The modified string.
+  * Return: the modified string
   */
 char *cap_string(char *s)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (s[i])
 	{
-		if (i == 0 || s[i - 1] == ' ' || s[i - 1] == '\t' || s[i - 1] == '\n' || s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' || s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' || s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' || s[i - 1] == '}')
-		{
-			if (s[i] >= 'a' && s[i] <= 'z')
-			{
-				s[i] -= 32;
-			}
-		}
-		else
-		{
-			if (s[i] >= 'A' && s[i] <= 'Z')
-			{
-				s[i] += 32;
-			}
-		}
+		while (!(s[i] >= 'a' && s[i] <= 'z'))
+		i++;
+		if (s[i - 1] == ' ' ||
+		s[i - 1] == '\t' ||
+		s[i - 1] == '\n' ||
+		s[i - 1] == ',' ||
+		s[i - 1] == ';' ||
+		s[i - 1] == '.' ||
+		s[i - 1] == '!' ||
+		s[i - 1] == '?' ||
+		s[i - 1] == '"' ||
+		s[i - 1] == '(' ||
+		s[i - 1] == ')' ||
+		s[i - 1] == '{' ||
+		s[i - 1] == '}' ||
+		i == 0)
+			s[i] -= 32;
+
+		i++;
 	}
 
 	return (s);
