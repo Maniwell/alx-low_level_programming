8fced1008885a965a62f2ab22af0e3b05c8f323e
Maniwell <emailtoamanuel@gmail.com>
2023-07-13 20:39:48 +0000

    Added mandatory tasks for 0x0C-more_malloc_free project ALX SE

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+  * _calloc - Allocates memory for an array using malloc
+  * @nmemb: The number of elements in the array
+  * @size: The size of each element in bytes
+  *
+  * Return: A pointer to the allocated memory, or NULL if malloc fails
+  */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+	unsigned int i;
+	char *p;
+
+	if (nmemb == 0 || size == 0)
+	return (NULL);
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+	return (NULL);
+	p = ptr;
+	for (i = 0; i < nmemb * size; i++)
+	p[i] = 0;
+
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+This directory contains files related to project for 0x0C-more malloc free ALX SE. 
